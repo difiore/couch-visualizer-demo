b04901466c476958d96883fc69aad8bda967accc
Anthony Di Fiore <anthony.difiore@gmail.com>
2015-05-04 14:04:50 -0500

    update server

---
+++ couch.js
@@ -0,0 +1,109 @@
+var record = {};
+
+function setup() {
+  // set a clock to update the Date/Time field every second:
+  setInterval(clock, 1000);
+
+  // set event listeners for the buttons.
+  // First get the whole button div:
+  var buttonDiv = document.getElementById('buttonDiv');
+  // then get all the buttons in a list:
+  var buttons = buttonDiv.getElementsByTagName('*');
+  // the iterate over the list and add event listeners:
+  for (var b=0; b< buttons.length; b++) {
+    buttons[b].addEventListener('click', contactServer, false);
+  }
+
+  //  generate a record ID:
+  document.getElementById('uuid').value = generateUuid();
+}
+
+// this function updates the date/time field  once a second:
+function clock() {
+  var dateField = document.getElementById('date');
+  dateField.value = new Date();
+}
+
+function generateUuid() {
+  var username = document.getElementById('username').value;
+  var uuid = new Date().valueOf();
+
+  for (var c=0; c< username.length; c++) {
+    uuid += (username.charCodeAt(c));
+  }
+   uuid = uuid * (Math.PI * Math.random(1000000));
+   uuid = Math.floor(uuid);
+   return uuid;
+}
+
+// this function contacts the server:
+function contactServer() {
+  // get the method from the button clicked:
+  var method = event.target.innerHTML;
+
+  // get the record _id and name from the UI field:
+  var id = document.getElementById('uuid').value;
+  var rev = document.getElementById('rev').value;
+  var username = document.getElementById('username').value;
+
+  if (id) record._id = id;
+  if (rev) record._rev = rev;
+  if (username) record.name = username;
+
+  // set up the basic HTTP parameters:
+  var params = {
+    url: 'http://104.236.9.143:5984/pp_contacts/',
+    type: method,
+    success: callback,
+    contentType: 'application/json',
+    dataType: 'json',
+  }
+  // convert the data to a string for sending:
+  data = JSON.stringify(record);
+
+  // GET wants /document/recordID and no request body:
+  if (method === 'GET') {
+    params.url += record._id;
+  }
+
+  // POST wants /document/ and a request body with record ID and record Rev:
+  if (method === 'POST') {
+    params.data = data;
+  }
+
+  // PUT wants /document/recordID and a request body with just the data
+  // (ID optional in the body):
+  if (method === 'PUT') {
+    params.url += record._id;
+    params.data = data;
+  }
+
+  // DELETE wants /document/recordID?recordRev and no request body:
+  if (method === 'DELETE') {
+    params.url += record._id;
+    params.url += "?rev=" + record._rev;
+  }
+
+  // make the HTTP call:
+  $.ajax(params);
+  console.dir(data);
+}
+
+// this function gets the request response:
+function callback(data) {
+  // save the response as the local record:
+  record = data;
+  // if there's a name, put it in the name field;
+  if (record.name) {
+    document.getElementById('username').value = record.name;
+  }
+  // if there's rev or a _rev, put it in the rev field:
+  if (record.rev) {
+    document.getElementById('rev').value = record.rev;
+  }
+  if (record._rev) {
+    document.getElementById('rev').value = record._rev;
+  }
+
+  console.dir(record);
+}
